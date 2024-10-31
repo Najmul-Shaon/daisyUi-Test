ed4203834a125c42dc7242efa30fc4a009f2621a
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-10-31 22:47:25 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight underline text-center">Hello world!</h1>
+      <button className="btn btn-warning flex flex-col items-center justify-center">Warning</button>
+    </div>
+  );
+}
+
+export default App;
